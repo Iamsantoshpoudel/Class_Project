875e113b9eaa185296eb23050f15178385c6fb11
Iamsantoshpoudel <santoshpoudel06.com.np@gmail.com>
2025-06-24 07:56:37 +0545

    Add About and Not Found pages, update layout metadata
    Created an About page and a custom Not Found page. Moved the Home page to a new directory structure. Updated layout metadata to reflect project details.

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container text-6xl text-center flex justify-center relative top-50 m-auto'>
+      
+      <h1>THIS IS ABOUT PAGE </h1>
+    </div>
+  )
+}
+
+export default About
---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+const Notfound = () => {
+  return (
+    <div className=" text-9xl text-blue-400 text-center  flex justify-center relative top-50 m-auto ">
+      OUT OF SLABOUS
+    </div>
+  );
+};
+
+export default Notfound;
